27d86d8e66eca1505155b71903728fd442992e72
Mike Bell <m.bell@techsmith.com>
2019-12-03 16:13:51 -0500

    Day3 Part2

--- 03/worker.js
+++ 03/worker.js
@@ -9,17 +9,28 @@ const pointsFromCommands = commands => {
         'D': () => y--
     };
 
+    let totalDistance = 0;
     commands.forEach(c => {
         const incrementer = directionsToIncrementers[c.direction];
         for (let i = 0; i < c.distance; i++) {
+            totalDistance++;
             incrementer();
-            points.push([x, y]);
+            points.push([x, y, totalDistance]);
         }
     });
 
     return points;
 };
 
-const findIntersections = (s1, s2) => s1.filter(p1 => s2.some(p2 => p1[0] == p2[0] && p1[1] == p2[1]));
+const getFirstMatchingPoint = (point, points) => points.find(p2 => point[0] == p2[0] && point[1] == p2[1]);
 
-module.exports = {pointsFromCommands, findIntersections};
+const findIntersections = (s1, s2) => s1.filter(p1 => getFirstMatchingPoint(p1, s2));
+
+const findItersectionTotalDistances = (s1, s2) => s1.map(p1 => {
+        const hit = getFirstMatchingPoint(p1, s2);
+        return {intersects: !!hit, distance: hit ? hit[2] + p1[2] : NaN};
+    })
+    .filter(hit => hit.intersects)
+    .map(hit => hit.distance);
+
+module.exports = {pointsFromCommands, findIntersections, findItersectionTotalDistances};
