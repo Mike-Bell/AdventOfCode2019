9ad5f7850e0e4dde3617d197254d1bcc22ee425c
Mike Bell <mikel3377@gmail.com>
2019-12-04 20:32:37 -0500

    Make part 1 even more functional

--- 04/solution1.js
+++ 04/solution1.js
@@ -1,10 +1,10 @@
 const range = require('../utils/range');
 
-const run = passwordRange => range(passwordRange[0], passwordRange[1]).filter(password => {
-    const windows = ('' + password).split('')
-        .reduce((acc, _, i, o) => i > 0 ? [...acc, o.slice(i - 1, i + 1)] : acc, []);
-
-    return windows.some(w => w[0] == w[1]) && !windows.some(w => w[0] > w[1]);
-}).length;
+const run = passwordRange => range(passwordRange[0], passwordRange[1])
+    .filter(password => ('' + password).split('')
+        .reduce((acc, _, i, o) => i > 0 ? [...acc, o.slice(i - 1, i + 1)] : acc, [])
+        .reduce((acc, w) => [acc[0] || w[0] == w[1], acc[1] && w[1] >= w[0]], [false, true])
+        .every(ele => ele)
+).length;
 
 module.exports = {run};
