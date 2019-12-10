9e38f7c7309a26219d3ef8f2a185c4960ab37487
Mike Bell <m.bell@techsmith.com>
2019-12-10 14:13:06 -0500

    Day10 part 2

--- 10/solution.js
+++ 10/solution.js
@@ -1,6 +1,6 @@
 const parseInput = input => input.split('\n').map(line => line.split(''));
 
-const slopBetweenAsteroids = (a1, a2) => (a2.y - a1.y) / (a2.x - a1.x);
+const slopeBetweenAsteroids = (a1, a2) => (a2.y - a1.y) / (a2.x - a1.x);
 
 const areSameAsteroid = (a1, a2) => a2.x === a1.x && a2.y === a1.y;
 
@@ -26,11 +26,11 @@ const getMaximallyObservantViewData = asteroids => {
                 return false;
             }
 
-            const slope = slopBetweenAsteroids(a1, a2);
+            const slope = slopeBetweenAsteroids(a1, a2);
             const [y2, y1] = a2.y > a1.y ?  [a2.y, a1.y] : [a1.y, a2.y];
             const [x2, x1] = a2.x > a1.x ?  [a2.x, a1.x] : [a1.x, a2.x];
             const candidateBlockers = asteroids.filter(a3 => asteroidIsBetweenBounds(y1, y2, x1, x2, a3) && !areSameAsteroid(a3, a2) && !areSameAsteroid(a3, a1));
-            return !candidateBlockers.some(a3 => slopBetweenAsteroids(a1, a3) === slope);
+            return !candidateBlockers.some(a3 => slopeBetweenAsteroids(a1, a3) === slope);
         }).length;
         return {x: a1.x, y: a1.y, views: views};
     });
@@ -52,6 +52,58 @@ const runPart1 = grid => {
 const runPart2 = grid => {
     const asteroids = getAsteroids(grid);
     const station = getMaximallyObservantViewData(asteroids);
+    const targets = asteroids.map(a => {
+        const angle = (450 + Math.atan2(a.y - station.y, a.x - station.x) * 180 / Math.PI) % 360;
+        return {x: a.x, y: a.y, angle: angle, hit: false};
+    }).filter(t => t.x !== station.x || t.y !== station.y);
+
+    let angle = null;
+    let first = true;
+    const getNextHit = () => {
+        let lowestDistance = null;
+        let lowestTarget = null;
+        targets.forEach(t => {
+            if (t.hit) {
+                return;
+            }
+
+            const rotationToTarget = (t.angle - angle + 360) % 360;
+
+            if (!first && rotationToTarget === 0) {
+                return;
+            }
+
+            if (lowestDistance === null) {
+                lowestDistance = rotationToTarget;
+                lowestTarget = t;
+                return;
+            }
+
+            if (rotationToTarget < lowestDistance) {
+                lowestTarget = t;
+                lowestDistance = rotationToTarget;
+                return;
+            }
+
+            if (lowestDistance === rotationToTarget && Math.abs(t.x - station.x) < Math.abs(lowestTarget.x - station.x)) {
+                lowestTarget = t;
+                return;
+            }
+        });
+
+        return lowestTarget;
+    };
+    
+    let hits = 0;
+    let target = null;
+    while (hits < 200) {
+        target = getNextHit();
+        first = false;
+        target.hit = true;
+        angle = target.angle;
+        hits++;
+    }
+    return target.x * 100 + target.y;
 };
 
 module.exports = {parseInput, runPart1, runPart2};
