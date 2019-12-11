80f8e1a9fa0247d2355b932fa4b325002a34b944
Mike Bell <m.bell@techsmith.com>
2019-12-11 10:30:28 -0500

    Day 11 part 2

--- 11/solution.js
+++ 11/solution.js
@@ -1,8 +1,8 @@
 const parseInput = input => input.split(',').map(n => +n);
 const computerFactory = require('../shared/computer');
+const range = require('../utils/range');
 
-const runPart1 = arr => {
-    const grid = [];
+const runGridToCompletion = (grid, arr) => {
     let coords = [0, 0];
     let direction = [0, 1];
     const rotateDirection = n => {
@@ -27,6 +27,12 @@ const runPart1 = arr => {
         coords = [x + direction[0], y + direction[1]];
     }
 
+    return grid;
+};
+
+const runPart1 = arr => {
+    const grid = runGridToCompletion([], arr);
+
     let total = 0;
     for (const x in grid) {
         for (const y in grid[x]) {
@@ -37,4 +43,32 @@ const runPart1 = arr => {
     return total;
 };
 
-module.exports = {parseInput, runPart1};
+const runPart2 = arr => {
+    const grid = runGridToCompletion([[1]], arr);
+    let lowestX = 0;
+    let lowestY = 0;
+    let highestX = 0;
+    let highestY = 0;
+    for (const sx in grid) {
+        const x = +sx;
+        lowestX = x < lowestX ? x : lowestX;
+        highestX = x > highestX ? x : highestX;
+        for (const sy in grid[x]) {
+            const y = +sy;
+            lowestY = y < lowestY ? y : lowestY;
+            highestY = y > highestY ? y : highestY;
+        }
+    }
+
+    let img = '';
+    range(lowestY, highestY + 1).reverse().forEach(y => {
+        range(lowestX, highestX + 1).forEach(x => {
+            img += (grid[x] || [])[y] ? '#' : ' ';
+        });
+        img += '\n';
+    });
+
+    return img;
+};
+
+module.exports = {parseInput, runPart1, runPart2};
