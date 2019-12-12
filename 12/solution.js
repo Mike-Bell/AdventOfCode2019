d01083dfbdfd9094b8792998e06fd77a8b516d6f
Mike Bell <m.bell@techsmith.com>
2019-12-12 11:04:43 -0500

    day 12 cleanup

--- 12/solution.js
+++ 12/solution.js
@@ -6,23 +6,27 @@ const lcm = require('../utils/lcm');
 const parseInput = input => input.split('\n').map(line => ({
     position: line.split(',').map(chunk => +chunk.replace(/(x|y|z|<|>|=|\s)/g, '')),
     velocity: [0, 0, 0]
-})); 
+}));
+
+const runStep = moons => {
+    moons.forEach((m1, i) => {
+        moons.forEach((m2, j) => {
+            if (i === j) {
+                return;
+            }
+
+            m1.velocity = m1.velocity.map((v, v1) => v + clamp(m2.position[v1] - m1.position[v1], -1, 1));
+        });
+    });
+
+    moons.forEach(m => {
+        m.position = m.position.map((p, pi) => p + m.velocity[pi]);
+    });
+};
 
 const runPart1 = moons => {
     range(1000).forEach(_ => {
-        moons.forEach((m1, i) => {
-            moons.forEach((m2, j) => {
-                if (i === j) {
-                    return;
-                }
-
-                m1.velocity = m1.velocity.map((v, v1) => v + clamp(m2.position[v1] - m1.position[v1], -1, 1));
-            });
-        });
-
-        moons.forEach(m => {
-            m.position = m.position.map((p, pi) => p + m.velocity[pi]);
-        });
+        runStep(moons);
     });
 
     return sum(moons.map(m => sum(m.position.map(Math.abs)) * sum(m.velocity.map(Math.abs))));
@@ -34,29 +38,10 @@ const runPart2 = moons => {
 
     const moonIsInIitialState = (m, mi, axis) => m.position[axis] === initial[mi].position[axis] && m.velocity[axis] === initial[mi].velocity[axis];
 
-    let i = 0;
-    while(true) {
-        moons.forEach((m1, i) => {
-            moons.forEach((m2, j) => {
-                if (i === j) {
-                    return;
-                }
-
-                m1.velocity = m1.velocity.map((v, v1) => v + clamp(m2.position[v1] - m1.position[v1], -1, 1));
-            });
-        });
-
-        i++;
-
-        moons.forEach(m => {
-            m.position = m.position.map((p, pi) => p + m.velocity[pi]);
-        });
+    for (let i = 1; !periods.every(p => p); i++) {
+        runStep(moons);
 
         periods = periods.map((p, axis) => p || (moons.every((m, mi) => moonIsInIitialState(m, mi, axis)) ? i : 0));
-
-        if (periods.every(p => p)) {
-            break;
-        }
     }
 
     return periods.reduce(lcm);
